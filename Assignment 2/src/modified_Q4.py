41cb9d4f42b7cd913748b0febc4250fb17cad8ec
codedecde <barunpatra95@gmail.com>
2015-09-24 15:53:51 +0530

    TODO: Pretty Printing

--- Assignment 2/src/modified_Q4.py
+++ Assignment 2/src/modified_Q4.py
@@ -2,23 +2,31 @@ import sys, socket, json, time, thread, threading
 
 class handle_objects(threading.Thread):
 	def __init__(self,domain_name,list_of_objects):
+		threading.Thread.__init__(self)
 		self.domain = domain_name
 		self.list_of_objects = list_of_objects
 
 	def run(self):
-		#TODO: Parallelise The object download.
+		#TODO: Pretty Printing.
 		connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 		connection.connect((self.domain,80))
-		for request in self.list_of_objects:
+		for idx in range(0,len(self.list_of_objects)):
+			request = self.list_of_objects[idx]
 			all_data = ''
-			request_string = "GET " + request + " HTTP/1.1\r\n\r\n"
+			if(idx != len(self.list_of_objects) -1):
+				request_string = "GET " + request + " HTTP/1.1\r\nHost: " + self.domain+ "\r\n\r\n"
+			else:
+				request_string = "GET " + request + " HTTP/1.1\r\nHost: " + self.domain+ "\r\nConnection: close\r\n\r\n"
 			connection.send(request_string)
+			#f = open(request,'w')
 			while(True):
 				data = connection.recv(1024)
 				if(len(data) == 0):
 					break
 				else:
 					all_data = all_data + data 
+			print all_data
+			#f.write(all_data)
 			#Store all_data somewhere.
 		connection.close()
 
@@ -26,6 +34,7 @@ class handle_objects(threading.Thread):
 
 class handle_domain(threading.Thread):
 	def __init__(self,domain_name,list_of_objects,maxTCP,maxOBJ):
+		threading.Thread.__init__(self)
 		self.domain = domain_name
 		self.list_of_objects = list_of_objects
 		self.maxTCP = maxTCP
@@ -37,18 +46,18 @@ class handle_domain(threading.Thread):
 			thread_list = []
 			for i in range(0,self.maxTCP):
 				high = self.maxOBJ + low
-				if(high > len(list_of_objects)):
-					high = len(list_of_objects)
+				if(high > len(self.list_of_objects)):
+					high = len(self.list_of_objects)
 				thread = handle_objects(self.domain,self.list_of_objects[low:high])
 				thread.start()
 				thread_list.append(thread)
 				low = high
-				if(len(list_of_objects) == low):
+				if(len(self.list_of_objects) == low):
 					break
 			#So that only maxTCP connections are active at any given time.
 			for thread in thread_list:
 				thread.join()
-			if(low == len(list_of_objects)):
+			if(low == len(self.list_of_objects)):
 				break
 
 
@@ -64,11 +73,11 @@ class object_Tree_Handler:
 			self.maxdepth = 0
 		
 		for line in f:
-			words = line.split()
+			words = line.split(',')
 			current_id = eval(words[0])
 			parent_id = eval(words[2])
 			if(parent_id not in id_to_level):
-				id_to_level[parent_id] = 0
+				id_to_level[parent_id] = -1
 			current_level = id_to_level[parent_id] + 1
 			id_to_level[current_id] = current_level
 			
@@ -76,20 +85,20 @@ class object_Tree_Handler:
 			if(current_level > self.maxdepth):
 				self.maxdepth = current_level
 
-	def setConstants(max_TCP_per_domain,max_objects_per_tcp):
+	def setConstants(self,max_TCP_per_domain,max_objects_per_tcp):
 		self.maxTCP = max_TCP_per_domain
 		self.maxOBJ = max_objects_per_tcp
 	
 	def getLevelOfTree(self,level):
 		domain_map = {}
 		for object in self.objects_per_level[level]:
-			key = object.lstrip('http://').split('/')[0]
+			key = object.split('/')[2]
 			if(key not in domain_map):
 				domain_map[key] = [object]
 			else:
 				domain_map[key].append(object)
 		thread_list = [] 
-		for key domain_map.keys():
+		for key in domain_map.keys():
 			thread = handle_domain(key,domain_map[key],self.maxTCP,self.maxOBJ)
 			thread.start()
 			thread_list.append(thread)
@@ -102,5 +111,9 @@ class object_Tree_Handler:
 		for i in range(0,self.maxdepth):
 			self.getLevelOfTree(i)
 	
+
+x = object_Tree_Handler('../dumps/www.vox.com.objt')
+x.setConstants(2,2)
+x.getLevelOfTree(1)
 
 
