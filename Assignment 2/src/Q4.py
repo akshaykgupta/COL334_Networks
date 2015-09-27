3df16c652e589aa800aec530ca33f34a046733dd
Akshay Kumar Gupta <akshay.akgupta@gmail.com>
2015-09-27 14:20:26 +0530

    renamed modified_q4 to q4

--- Assignment 2/src/Q4.py
+++ Assignment 2/src/Q4.py
@@ -1,54 +1,132 @@
-import sys, socket, json, time, thread, threading
+import sys, socket, json, time, thread, threading, os, mutex
 
-list_of_requests = [] # list of dictionary objects.
+mutex filemutex = mutex()
 
-#threading for TCP sockets:
+def get_filestream( fullpath ):
+	global.filemutex #Is this needed?
+	directory = os.path.dirname(filename) #Will not having a directory part create a problem? Like, will directory = "" create problems?
+	if not os.path.exists(directory):
+		filemutex.lock(os.makedirs , directory)
+		filemutex.unlock()
+	f = open(fullpath)
+	return f
 
-class tcp_thread(threading.Thread):
-	def __init__(self, threadID, start, end):
-		threading.Thread.__init(self)
-		self.start = start
-		self.end = end
-		self.threadID = threadID
-		
-	def processObject(obj):
-		resultString = "GET " + SOMETHING + " HTTP1.1\r\nConnection:keep-alive\r\n\r\n"
-		return resultString
+class handle_objects(threading.Thread):
+	def __init__(self,domain_name,list_of_objects):
+		threading.Thread.__init__(self)
+		self.domain = domain_name
+		self.list_of_objects = list_of_objects
+
+	def run(self):
+		#TODO: Pretty Printing.
+		connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+		connection.connect((self.domain,80))
+		for idx in range(0,len(self.list_of_objects)):
+			request = self.list_of_objects[idx]
+			all_data = ''
+			if(idx != len(self.list_of_objects) -1):
+				request_string = "GET " + request + " HTTP/1.1\r\nHost: " + self.domain+ "\r\n\r\n"
+			else:
+				request_string = "GET " + request + " HTTP/1.1\r\nHost: " + self.domain+ "\r\nConnection: close\r\n\r\n"
+			connection.send(request_string)
+			#f = open(request,'w')
+			while(True):
+				data = connection.recv(1024)
+				if(len(data) == 0):
+					break
+				else:
+					all_data = all_data + data 
+			f = get_filestream
+			#print all_data
+			f.write(all_data)
+			f.close() #Is this necessary?
+			#Store all_data somewhere.
+		connection.close()
+
+			
+
+class handle_domain(threading.Thread):
+	def __init__(self,domain_name,list_of_objects,maxTCP,maxOBJ):
+		threading.Thread.__init__(self)
+		self.domain = domain_name
+		self.list_of_objects = list_of_objects
+		self.maxTCP = maxTCP
+		self.maxOBJ = maxOBJ
 	
 	def run(self):
-		#I need to start a socket, then close it later.
-		global list_of_requests
-		connection = socket.socket(AF_INET , SOCK_STREAM, 0) #a tcp socket.
-		#IMPORTANT NOTE : For the last Request, Connection header needs to be "close"
-		for idx in range(self.start,self.end):
-			request = processObject(list_of_requests[idx])
-			portno = 80 # Check
-			connection.connect((request,portno))
+		low = 0
+		while(True):
+			thread_list = []
+			for i in range(0,self.maxTCP):
+				high = self.maxOBJ + low
+				if(high > len(self.list_of_objects)):
+					high = len(self.list_of_objects)
+				thread = handle_objects(self.domain,self.list_of_objects[low:high])
+				thread.start()
+				thread_list.append(thread)
+				low = high
+				if(len(self.list_of_objects) == low):
+					break
+			#So that only maxTCP connections are active at any given time.
+			for thread in thread_list:
+				thread.join()
+			if(low == len(self.list_of_objects)):
+				break
 
 
+class object_Tree_Handler:
+	def __init__(self,filename,maxdepth = 0):
+		f = open(filename)
+		id_to_level = {}
+		if(maxdepth != 0):
+			self.maxdepth = maxdepth
+			self.objects_per_level = [[] for it in range(0,self.maxdepth)]  
+		else:
+			self.objects_per_level = [[] for it in range(0,10)] #Assuming max Tree Depth to be 10
+			self.maxdepth = 0
+		
+		for line in f:
+			words = line.split(',')
+			current_id = eval(words[0])
+			parent_id = eval(words[2])
+			if(parent_id not in id_to_level):
+				id_to_level[parent_id] = -1
+			current_level = id_to_level[parent_id] + 1
+			id_to_level[current_id] = current_level
+			
+			self.objects_per_level[current_level].append(words[1])
+			if(current_level > self.maxdepth):
+				self.maxdepth = current_level
+
+	def setConstants(self,max_TCP_per_domain,max_objects_per_tcp):
+		self.maxTCP = max_TCP_per_domain
+		self.maxOBJ = max_objects_per_tcp
+	
+	def getLevelOfTree(self,level):
+		domain_map = {}
+		for object in self.objects_per_level[level]:
+			key = object.split('/')[2]
+			if(key not in domain_map):
+				domain_map[key] = [object]
+			else:
+				domain_map[key].append(object)
+		thread_list = [] 
+		for key in domain_map.keys():
+			thread = handle_domain(key,domain_map[key],self.maxTCP,self.maxOBJ)
+			thread.start()
+			thread_list.append(thread)
+
+		for thread in thread_list:
+			thread.join()
 
 
-#Things I need for a HTTP Get request are :
-
-
-
-def extract_requests_har(infile):
-	f = open(infile) #open the file!
-	har_data = json.parse(f)['log']
-	#entries in har_data are : version, creator, browser, pages, entries
+	def getTree(self):
+		for i in range(0,self.maxdepth):
+			self.getLevelOfTree(i)
 	
 
-def main(infile, n_tcp, n_obj_per_tcp):
-	if ( sys.argv[1]="har" ):
-		extract_requests_har(infile)
-	elif ( sys.argv[1]="obj" ):
-		extract_requests_obj(infile)
-	else:
-		sys.exit("Error! Wrong input mode! Please use har for .har file and obj for object trees!\n") #exit due to error.
-
-	#ASSERT : we have list of requests.
+x = object_Tree_Handler('../dumps/www.vox.com.objt')
+x.setConstants(2,2)
+x.getLevelOfTree(1)
 
 
-
-if __name__ == '__main__':
-	main(sys.argv[2] , sys.argv[3], sys.argv[4]) #argv[2] is HAR file/ OBJT file. argv[3] is #of tcp connections. argv[4] is number of objects per connection.
