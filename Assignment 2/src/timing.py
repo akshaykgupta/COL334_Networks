25faefcd4d4d0bcea7df560f231488b4cc8849c1
codedecde <barunpatra95@gmail.com>
2015-10-02 20:48:43 +0530

    Timing analysis

--- Assignment 2/src/timing.py
+++ Assignment 2/src/timing.py
@@ -1,31 +1,47 @@
-import os,Q4,sys
-from sys import platform as operating_system
+import os,Q4,sys,time
+from sys import platform as pf
+
+if(len(sys.argv) != 2):
+	print 'Syntax for correct usage: python timing.py <HAR file/OBJ Tree source>'
+	exit()
 
 #Edit these:
 max_tcp = 8
 max_obj = 8 
-f = open("timing_analysis.data" , "a")
-
+f = open("timing_analysis.data" , "w")
+f.write('Max TCP \t Max OBJ \t Time Taken')
 for tcp in range(1,max_tcp):
 	for obj in range(1,max_obj):
-
-		FLUSH DNS # IMPORTANT !!!!!!!! USE SYSTEM KA FLUSH DNS
-		if operating_system == "linux":
-			#TODO
-		else if operating_system == "win32":
+		# Flushing the DNS cache
+		operating_system = pf.system()
+		if (operating_system == "Linux"):
+			os.system("rndc restart")
+		elif operating_system == "Windows":
 			os.system("ipconfig /flushdns")
-		else if operating_system == "darwin":
-			#TODO
-		Q4.index_file = "mapping_for_" + str(tcp) + "_" + str(obj) + ".txt"
-		Q4.directory_name = "dl_" + str(tcp) + "_" + str(obj)
-		#START TIME
+		elif operating_system == "Darwin":
+			os.system("dscacheutil -flushcache")
+		else:
+			print 'Undrecognized OS. Quitting...'
+			exit()
+		
+		#.idx for mapping index files.
+		index_file = "mapping_for_" + str(tcp) + "_" + str(obj) + ".idx"
+		directory_name = "dl_" + str(tcp) + "_" + str(obj)
+		
+		if(not os.path.exists(directory_name)):
+			os.makedirs(directory_name)
+		
+		the_downloader = Q4.downloader()
+		
+		the_downloader.set_constants(index_file,directory_name)
+		#....Starting......
 		start = time.time()
-		x = object_Tree_Handler(sys.argv[1])
+		x = the_downloader.object_Tree_Handler(sys.argv[1])
 		x.set_max_values(tcp,obj)
 		x.get_tree()
 		x.post_process()
-		#END TIME
+		#....Ending......
 		time_taken = time.time() - start
-		#WRITE INTO FILE
+		#Writing to file
 		f.write( str(tcp) + "\t" + str(obj) + "\t" + str(time_taken))
 f.close()
